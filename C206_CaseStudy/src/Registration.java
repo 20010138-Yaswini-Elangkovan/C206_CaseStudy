78f29bc8666041d33e6255c355c0021734c69d87
20010138 <200010138@myrp.edu.sg>
2021-08-03 20:21:05 +0800

    Wini Was here

---
+++ C206_CaseStudy/src/Registration.java
@@ -0,0 +1,40 @@
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+/**
+ * I declare that this code was written by me. 
+* I will not copy or allow others to copy my code. 
+* I understand that copying code is considered as plagiarism. 
+* yaswini, Aug 3, 2021 7:26:25 PM
+ */
+
+/**
+ * @author Yaswini
+ *
+ */
+public class Registration {
+	
+	private int rgNo;
+	private int tuitimeID;
+	private String stEmail;
+	private String status;
+	private String date;
+	private String time; 
+	private int rgID; 
+	
+	public Registration(int rgNo, int tuitimeID, String stEmail, String status, String date, String time,
+			int rgID) {
+	
+		this.rgNo = rgNo;
+		this.tuitimeID = tuitimeID;
+		this.stEmail = stEmail;
+		this.status = status;
+		this.date = date;
+		this.time = time;
+		this.rgID = rgID;
+	}
+
+	
+	
+
+}
