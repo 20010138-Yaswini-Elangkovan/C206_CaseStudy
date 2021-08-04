15476933537ce35a8c4d68ed6ce30171ba4c5d2c
20041881 <20041881@LAPTOP-4UF1O56H>
2021-08-04 13:26:41 +0800

    Aini did this

--- C206_CaseStudy/src/TuitionManagementSystem.java
+++ C206_CaseStudy/src/TuitionManagementSystem.java
@@ -9,34 +9,68 @@ public class TuitionManagementSystem {
 	public static void main(String[] args) {
 		ArrayList<Timetable> timetableList = new ArrayList<Timetable>();
 		ArrayList<Timetable> registrationList = new ArrayList<Timetable>();
-		Administrator admin = new Administrator("Admin", "Admin");
 		
+		Administrator admin = new Administrator("Admin", "Admin");
+		Student student = new Student("Student", "Student");
+		Manager manager = new Manager("Manager", "Manager");
+		
+		// LOGIN PAGE
 		int option = 0;
 		while (option != OPTION_QUIT) {
 			TuitionManagementSystem.menu();
 			option = Helper.readInt("Login as > ");
-		// LOGIN PAGE
-		if (option == 2) {
-			boolean isCorrect = false; 
-			
-			if (isCorrect == false) {
-				String Username = Helper.readString("Username >  ");
-				String Password = Helper.readString("Password >  ");
+			if (option == 1) {
+				boolean isCorrect1 = false; 
 				
-				if(admin.getUsername().equals(Username) && admin.getPassword().equals(Password)) {
-					isCorrect = true;
+				if (isCorrect1 == false) {
+					String Username1 = Helper.readString("Username >  ");
+					String Password1 = Helper.readString("Password >  ");
+					
+					if(student.getUsername().equals(Username1) && student.getPassword().equals(Password1)) {
+						isCorrect1 = true;
+					} else {
+						System.out.println("Username or password is incorrect, please try again!");
+					}
+					Helper.line(80, "-");
+				}
+			
+		if (option == 2) {
+			boolean isCorrect2 = false; 
+			
+			if (isCorrect2 == false) {
+				String Username2 = Helper.readString("Username >  ");
+				String Password2 = Helper.readString("Password >  ");
+				
+				if(admin.getUsername().equals(Username2) && admin.getPassword().equals(Password2)) {
+					isCorrect2 = true;
 				} else {
 					System.out.println("Username or password is incorrect, please try again!");
 				}
 				Helper.line(80, "-");
+			}
+				if (option == 3) {
+					boolean isCorrect3 = false; 
+					
+					if (isCorrect3 == false) {
+						String Username3 = Helper.readString("Username >  ");
+						String Password3 = Helper.readString("Password >  ");
+						
+						if(manager.getUsername().equals(Username3) && manager.getPassword().equals(Password3)) {
+							isCorrect3 = true;
+						} else {
+							System.out.println("Username or password is incorrect, please try again!");
+						}
+						Helper.line(80, "-");
+					}
+				
 			}else if (option == OPTION_QUIT) {
 				System.out.println("Bye!");
 			} else {
 				System.out.println("Invalid option");
 			}
-			
+		}
+		
 			//administrator menu
-				
 		int adminOption = 0;
 		while (adminOption != admin_QUIT) {
 			TuitionManagementSystem.adminmenu();
@@ -66,7 +100,7 @@ public class TuitionManagementSystem {
 				System.out.println("THANKS FOR USING TUITION MANAGEMENT CENTRE!");
 
 			}
-			}
+		}
 		}else if (option == admin_QUIT) {
 			System.out.println("Bye!");
 		} else {
@@ -75,14 +109,11 @@ public class TuitionManagementSystem {
 		
 	
 			
-			
-			
+}
+}
 		
 		
 	
-
-		}
-		}
 		
 	
 
