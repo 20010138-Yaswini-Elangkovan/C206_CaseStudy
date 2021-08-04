b60cca277f31ba92370ed4b7a0c89586f2d23442
20041881 <20041881@LAPTOP-4UF1O56H>
2021-08-04 13:26:12 +0800

    Aini did this

---
+++ C206_CaseStudy/src/Manager.java
@@ -0,0 +1,17 @@
+
+public class Manager {
+	private String Username;
+	private String Password;
+	
+	public Manager(String Username, String Password) {
+	this.Username = Username;
+	this.Password = Password;
+}
+	public String getUsername() {
+		return Username;
+	}
+	public String getPassword() {
+		return Password;
+	}
+}
+
