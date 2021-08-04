de4291cb5715aaedf5b2b8e281b6092c50397737
20041881 <20041881@myrp.edu.sg>
2021-08-04 13:12:26 +0800

    Aini did this

---
+++ C206_CaseStudy/src/Student.java
@@ -0,0 +1,17 @@
+
+public class Student {
+	private String Username;
+	private String Password;
+	
+	public Student(String Username, String Password) {
+	this.Username = Username;
+	this.Password = Password;
+}
+	public String getUsername() {
+		return Username;
+	}
+	public String getPassword() {
+		return Password;
+	}
+}
+
