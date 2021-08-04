bf144aafef546c8883b2dc16f8fd4d34b0590f6e
20010138 <200010138@myrp.edu.sg>
2021-08-04 13:44:36 +0800

    Manager class

---
+++ C206_CaseStudy/src/Manager.java
@@ -0,0 +1,18 @@
+
+public class Manager {
+	private String Username;
+	private String Password;
+	
+	public Manager(String Username, String Password) {
+	this.Username = Username;
+	this.Password = Password;
+}
+	public String getUsername() {
+		return Username;
+	}
+	public String getPassword() {
+		return Password;
+	}
+}
+
+//
